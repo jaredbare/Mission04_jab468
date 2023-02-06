370365b0e7c70c3bba6b3ce65d47c98f63306644
jaredbare <jab468@byu.edu>
2023-02-06 16:53:45 -0700

    MVC app changes

---
+++ Mission04_jab468/Controllers/BlahController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_jab468.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Calc()
+        {
+            return View();
+        }
+    }
+}
