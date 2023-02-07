71d702b53f8143f654a18368c78047aa7eab0440
jaredbare <jab468@byu.edu>
2023-02-06 21:30:15 -0700

    asp-for & Required

--- Mission04_jab468/Models/Calc.cs
+++ Mission04_jab468/Models/Calc.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,17 @@ namespace Mission04_jab468.Models
 {
     public class CalcModel
     {
+        [Required]
         public byte Assignments { get; set; }
+        [Required]
         public byte GroupProject { get; set; }
+        [Required]
         public byte Quizzes { get; set; }
+        [Required]
         public byte Midterm { get; set; }
+        [Required]
         public byte Final { get; set; }
+        [Required]
         public byte Intex { get; set; }
     }
 }
